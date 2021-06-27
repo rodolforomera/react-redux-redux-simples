72136fe84779f700caea630119d0ec612e643d04
rodolforomera <rodolforomeras@gmail.com>
2021-06-27 15:31:15 -0300

    Refatorado Reducer

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,31 +1,8 @@
 import { createStore, combineReducers } from 'redux';
+import numerosReducer from './reducers/numeros'
 
 const reducers = combineReducers({
-    numeros: function(state, action) {
-        switch(action.type) {
-            case 'NUM_MIN_ALTERADO':
-                return {
-                    ...state, min: action.payload
-
-                }
-            case 'NUM_MAX_ALTERADO':
-                return {
-                    ...state, max: action.payload
-
-                }
-            default: 
-                return {
-                    min: 7,
-                    max: 31
-                }
-        }
-    },
-    nomes: function(state, action) {
-        return [
-            'Ana',
-            'Bia'
-        ]
-    }
+    numeros: numerosReducer
 })
 
 function storeConfig() {
