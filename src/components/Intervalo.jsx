01d3a66b7aa2192e69fe45c260c7133da8cb9c4f
rodolforomera <rodolforomeras@gmail.com>
2021-06-27 14:09:07 -0300

    Implementação sem Redux

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -3,16 +3,21 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
+
+    const { min, max } = props;
+
     return (
         <Card title="Intervalo de Números" red>
             <div className="Intervalor">
                 <span>
                     <strong>Mínimo:</strong>
-                    <input type="number" value={0} readOnly/>
+                    <input type="number" value={min} 
+                        onChange={e => props.onMinChanged(+e.target.value)}/>
                 </span>
                 <span>
                     <strong>Máximo:</strong>
-                    <input type="number" value={10} readOnly/>
+                    <input type="number" value={max} 
+                        onChange={e => props.onMaxChanged(+e.target.value)}/>
                 </span>
             </div>
         </Card>
--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
+
+    const { min, max } = props;
+
     return (
         <Card title="Soma dos Números" blue>
             <div>
                 <span>
                     <span>Resultado:</span>
-                    <strong>10</strong>
+                    <strong>{ max + min}</strong>
                 </span>
             </div>
         </Card>
