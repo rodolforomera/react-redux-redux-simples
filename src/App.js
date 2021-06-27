6d012ddfe63b97b1ba910037042bf178c3303f21
rodolforomera <rodolforomeras@gmail.com>
2021-06-27 14:23:39 -0300

    Removida implementação sem redux e realizada configuração inicial do redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css';
 import Intervalo from './components/Intervalo'
 import Media from './components/Media'
@@ -7,21 +6,16 @@ import Sorteio from './components/Sorteio'
 
 function App() {
 
-  const [min, setMin] = useState(10);
-  const [max, setMax] = useState(20);
-
   return (
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className="linha">
-        <Intervalo min={min} max={max}
-          onMinChanged={setMin}
-          onMaxChanged={setMax}></Intervalo>
+        <Intervalo></Intervalo>
       </div>
       <div className="linha">
-        <Media min={min} max={max}></Media>
-        <Soma min={min} max={max}></Soma>
-        <Sorteio min={min} max={max}></Sorteio>
+        <Media></Media>
+        <Soma></Soma>
+        <Sorteio></Sorteio>
       </div>
     </div>
   );
